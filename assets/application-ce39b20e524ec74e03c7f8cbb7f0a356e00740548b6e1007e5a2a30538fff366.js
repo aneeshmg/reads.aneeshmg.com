e6e8a2dd92975e650dac99dea42ab23e1699a99f
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2018-05-16 17:46:49 +0530

    add GA id [ci skip]

---
+++ assets/application-ce39b20e524ec74e03c7f8cbb7f0a356e00740548b6e1007e5a2a30538fff366.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){"function"==typeof $.fn.scrollAppear&&$(".scrollappear").scrollAppear(),new Zooming({customSize:"100%",scaleBase:.9,scaleExtra:0,enableGrab:!1}).listen('img[data-action="zoom"]'),$(".article-share a").on("click",function(){return window.open($(this).attr("href"),"Share","width=200,height=200,noopener"),!1})});
+//# sourceMappingURL=/assets/source-maps/application.js.map
+//# sourceURL=_assets/javascripts/application.js
---
+++ assets/scrollappear-528db8b700e5eaed96d306068a0015dc8ab79672a5d7142d1efbaa4944a8f500.js
@@ -0,0 +1,3 @@
+!function(n){var t=[],i=function(){n.each(t,function(){n(this)[0].getBoundingClientRect().top<n(window).height()&&n(this).addClass("appeared")})};n.fn.scrollAppear=function(){return this.each(function(){t.push(n(this)),n(this).addClass("appear")}),i(),n(window).scroll(function(){i()}),this}}(jQuery);
+//# sourceMappingURL=/assets/source-maps/scrollappear.js.map
+//# sourceURL=_assets/javascripts/scrollappear.js
